1256abcb635a219c51321e9f323a095af9107aad
Aaditya Shah <ashah203@uottawa.ca>
2022-10-06 15:03:04 -0400

    bug fix

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -8,6 +8,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private double num1 = 0;
+    private double num2 = 0;
+
+    private char optr;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,52 +21,52 @@ public class MainActivity extends AppCompatActivity {
 
     public void btn01Click (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" 1");
+        etext.setText(etext.getText()+"1");
     }
 
     public void btn02Click (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText() + " 2");
+        etext.setText(etext.getText() + "2");
     }
 
     public void btn03Click (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" 3");
+        etext.setText(etext.getText()+"3");
     }
 
     public void btn04Click (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" 4");
+        etext.setText(etext.getText()+"4");
     }
 
     public void btn05Click (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" 5");
+        etext.setText(etext.getText()+"5");
     }
 
     public void btn06Click (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText() + " 6");
+        etext.setText(etext.getText() + "6");
     }
     public void btn07Click (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" 7");
+        etext.setText(etext.getText()+"7");
     }
     public void btn08Click (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" 8");
+        etext.setText(etext.getText()+"8");
     }
     public void btn09Click (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" 9");
+        etext.setText(etext.getText()+"9");
     }
     public void btn0Click (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" 0");
+        etext.setText(etext.getText()+"0");
     }
     public void btnDecimalClick (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" .");
+        etext.setText(etext.getText()+".");
     }
     public void btnClearClick (View view){
         EditText etext = findViewById(R.id.resultEdit);
@@ -69,42 +74,21 @@ public class MainActivity extends AppCompatActivity {
     }
     public void btnPlusClick (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" +");
+        etext.setText(etext.getText()+"+");
     }
     public void btnMinusClick (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" -");
+        etext.setText(etext.getText()+"-");
     }
     public void btnMultiplyClick (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" *");
+        etext.setText(etext.getText()+"*");
     }
     public void btnDivideClick (View view){
         EditText etext = findViewById(R.id.resultEdit);
-        etext.setText(etext.getText()+" /");
+        etext.setText(etext.getText()+"/");
     }
     public void btnEqualClick (View view){
-        EditText etext = findViewById(R.id.resultEdit);
-        String equation = etext.getText().toString();
-        String[] vars = etext.getText().toString().split(" ");
-        String temp1 = "";
-        String temp2 = "";
-        String tempOp = "";
-        for (int i = 0; i <vars.length ; i++) {
-            String val = vars[i];
-            if(!(vars[i].equals("+") || vars[i].equals("-") || vars[i].equals("/") || vars[i].equals("*"))){
-                if (tempOp.equals("")){
-                    temp1 +=  (vars[i]);
-                }
-                else {
-                    temp2+=(vars[i]);
-                }
-            }
-            else{
-                if(!temp2.equals("")){
-                    tempOp+=(vars[i]);
-                }
-            }
-        }
+
     }
 }
